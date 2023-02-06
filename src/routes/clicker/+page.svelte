e093817c98eafaca085fb58edb4198a017663436
0NI1 <moufar@edu.umea.se>
2023-02-06 09:45:33 +0100

    Major Changes (Font, Functions, ETC....)

--- src/routes/clicker/+page.svelte
+++ src/routes/clicker/+page.svelte
@@ -1,24 +1,46 @@
 <script>
-let cookies = 0;
+  import { src_url_equal } from "svelte/internal";
 
+let CPC = 0; // Cookies Per Click
+function cookieClicks(){
+    CPC += 1;
+    console.log(CPC)
+}
 </script>
- <h1>Welcome to ONI1'S Cookie Clicker!</h1> 
- 
+<h1>Welcome to ONI1'S Cookie Clicker!</h1> <!--Title on Clicker page -->
 
-<img class="Cookie" src="/CC.png"  alt="jhlkjh">
+<img id="CCI"class="Cookie" src="/CC.png" alt="Cookie Jummy :)"/> <!--Cookie picture -->
+
+<main>
+  <div>
+      <h2>Cookies: {CPC}</h2>
+      <button on:click={cookieClicks}>Button</button>
+  </div>
+</main>
 
 <style>
+
 .Cookie {
     width: 250px;
     height: 250px;
-    margin-left: auto;
-    margin-right: auto;
+    margin: auto;
+    display: block;
+    position: relative;
+    bottom: -100px;    
 }
 h1{
     text-shadow: 3px 3px 3px brown, 3px 3px 2px brown;
     font-weight: bold;
     text-align: center;
     font-size: 50px;
+    color:whitesmoke;
+}
+h2{
+  position: absolute;
+  top: 70px;
+  text-align: left;
+  color: brown;
+  text-shadow: 3px 3px 3px burlywood, 3px 3px 2px burlywood;
 }
 </style>
   

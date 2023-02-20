fd9833d0050d48538228bd3429de65872c4a34ee
0NI1 <moufar@edu.umea.se>
2023-02-20 09:45:47 +0100

    Added TODO tab with som minor changes :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,19 @@
 </script>
 <nav>
   <a href="/">Hem</a>
-  <a href="/telltale">Telltale</a>
   <a href="/clicker">Cookie Clicker</a>
-  <a href="/memory">Memory</a>
   <a href="/chat">Chat</a>
+  <a href="/todo">Chat</a>
+
+<!--
+  <a href="/telltale">Telltale</a>
+  <a href="/blog">Blog</a>
+  <a href="/memory">Memory</a>
   <a href="/search">Search</a>
   <a href="/blog">Blog</a>
+-->
 
 </nav>
 
-<hr />
 
 <slot />

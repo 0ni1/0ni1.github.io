c3a924e931029a3b4f8640ec9046089d1a91ac96
0NI1 <moufar@edu.umea.se>
2023-03-13 13:09:58 +0100

    Small changes in the code! :)

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,32 +1,35 @@
 <script>
 import { onMount } from 'svelte'; // For loading JSON data (load the TODO saved data)
-
-let newListItems = ""; 
-let TODOItems = []
-
-function addItemsToList() {
-
-
-}
-
+  
+  let newListItems = ""; 
+  
+  let TODOItems = []
+	
+	function addToList() {
+		TODOItems = [...TODOItems, {text: newListItems, status: false}];
+		newListItems = '';
+	}
+	
+	function removeFromList(index) {
+		TODOItems.splice(index, 1)
+		TODOItems = TODOItems;
+    }
 </script>
-<h3>0ni1's TODO list</h3>
-<button> class="(current === block1 ? )" </button>
-<head>
-    <style>
-    body {
-      background-color: #7E75FD;
-    }
-    nav{
-      background-color: #7E75FD;
-      
-    }
-    </style>
-  </head>
 
-<style>
-h3{
-    color:azure;
-}
- 
-</style>
+<input bind:value={newListItems} type="text" placeholder="new todo item..">
+<button on:click={addToList}>Add</button>
+
+<br/>
+{#each TODOItems as item, index}
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	<span on:keypress={() => removeFromList(index)}></span>
+	<br/>
+{/each} 
+
+
+<style> 
+	.checked {
+        text-decoration: line-through;
+    }
+</style> 
